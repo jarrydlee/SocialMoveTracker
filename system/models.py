9650aabdccc8817deee28b6d994c0364ef6702d7
Richard Yadon <richardfyadon@gmail.com>
2014-06-21 14:34:20 -0400

    Updates models

--- system/models.py
+++ system/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    post_id = models.CharField(max_length=50, unique=True)
+    text = models.CharField(max_length=250)
+    keyword = models.ForeignKey(Keyword)
+    label = models.CharField(max_length=5)
+    positive = models.IntegerField()
+    negative = models.IntegerField()
+    neutral = models.IntegerField()
+
+class Keyword(models.Model):
+    word = models.CharField(max_length=50)
