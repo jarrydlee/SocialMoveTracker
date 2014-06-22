f08d471868b84d855de08598d9c9545d42216267
jarrydlee <jrl5634@rit.edu>
2014-06-22 05:55:06 -0400

    LES GOOOOOOOOOOOOO

--- static/js/main.js
+++ static/js/main.js
@@ -10,7 +10,6 @@ var bindEvents = function () {
     getDoughnutData();
     getSidebar();
     getBarChartData();
-    getRadarGraph();
     // Loads all posts at the start
     loadPosts();
 
@@ -120,8 +119,8 @@ var getLineChartData = function (movieName) {
                 onAnimationComplete: null
             };
             var ctx = $('#hourlyChart')[0].getContext("2d");
-            ctx.canvas.width = 500;
-            ctx.canvas.height = 300;
+            ctx.canvas.width = 400;
+            ctx.canvas.height = 200;
             new Chart(ctx).Line(chartData, options);
         }
     });
@@ -140,7 +139,7 @@ var getSidebar = function() {
                     if (data[key] === 0) {
                         $('#movieSidebar').append('<li><a class="movie-choice"  id="' + movie + '" data-movie-title="' + key + '" href="#">' + key + '<span class=" text-success glyphicon glyphicon-circle-arrow-up pull-right"></span></a></li>')
                     } else if (data[key] === 1) {
-                        $('#movieSidebar').append('<li ><a class="movie-choice" id="' + movie + '" data-movie-title="' + key + '" href="#">' + key + '<span class=" text-success glyphicon glyphicon-circle-arrow-right pull-right"></span></a></li>')
+                        $('#movieSidebar').append('<li ><a class="movie-choice" id="' + movie + '" data-movie-title="' + key + '" href="#">' + key + '<span class=" text-success glyphicon glyphicon-circle-arrow-up pull-right"></span></a></li>')
                     } else {
                         $('#movieSidebar').append('<li><a class="movie-choice" id="' + movie + '" data-movie-title="' + key + '" href="#">' + key + '<span class=" text-danger glyphicon glyphicon-circle-arrow-down pull-right"></span></a></li>')
                     }
@@ -396,139 +395,8 @@ var getBarChartData = function () {
     }
     var ctx1 = $("#barGraph1").get(0).getContext("2d");
     var myNewChart1 = new Chart(ctx1).Bar(data1, options);
-    var ctx3 = $("#barGraph3").get(0).getContext("2d");
-    var myNewChart3 = new Chart(ctx3).Bar(data3, options);
 }
 
-var getRadarGraph = function () {
-    var data = {
-        labels: ["12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"],
-        datasets: [
-            {
-                fillColor: "rgba(151,187,205,0.5)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                data: [65, 59, 90, 81, 56, 55, 40, 65, 59, 90, 81, 56, 55,]
-            },
-            {
-                fillColor: "rgba(245,42,53,0.5)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                data: [28, 48, 40, 19, 96, 27, 100, 65, 59, 90, 81, 56, 55,]
-            }
-        ]
-    }
-    var options = {
-
-        //Boolean - If we show the scale above the chart data
-        scaleOverlay: false,
-
-        //Boolean - If we want to override with a hard coded scale
-        scaleOverride: false,
-
-        //** Required if scaleOverride is true **
-        //Number - The number of steps in a hard coded scale
-        scaleSteps: null,
-        //Number - The value jump in the hard coded scale
-        scaleStepWidth: null,
-        //Number - The centre starting value
-        scaleStartValue: null,
-
-        //Boolean - Whether to show lines for each scale point
-        scaleShowLine: true,
-
-        //String - Colour of the scale line
-        scaleLineColor: "rgba(0,0,0,.1)",
-
-        //Number - Pixel width of the scale line
-        scaleLineWidth: 1,
-
-        //Boolean - Whether to show labels on the scale
-        scaleShowLabels: false,
-
-        //Interpolated JS string - can access value
-        scaleLabel: "<%=value%>",
-
-        //String - Scale label font declaration for the scale label
-        scaleFontFamily: "'Arial'",
-
-        //Number - Scale label font size in pixels
-        scaleFontSize: 12,
-
-        //String - Scale label font weight style
-        scaleFontStyle: "normal",
-
-        //String - Scale label font colour
-        scaleFontColor: "#666",
-
-        //Boolean - Show a backdrop to the scale label
-        scaleShowLabelBackdrop: true,
-
-        //String - The colour of the label backdrop
-        scaleBackdropColor: "rgba(255,255,255,0.75)",
-
-        //Number - The backdrop padding above & below the label in pixels
-        scaleBackdropPaddingY: 2,
-
-        //Number - The backdrop padding to the side of the label in pixels
-        scaleBackdropPaddingX: 2,
-
-        //Boolean - Whether we show the angle lines out of the radar
-        angleShowLineOut: true,
-
-        //String - Colour of the angle line
-        angleLineColor: "rgba(0,0,0,.1)",
-
-        //Number - Pixel width of the angle line
-        angleLineWidth: 1,
-
-        //String - Point label font declaration
-        pointLabelFontFamily: "'Arial'",
-
-        //String - Point label font weight
-        pointLabelFontStyle: "normal",
-
-        //Number - Point label font size in pixels
-        pointLabelFontSize: 12,
-
-        //String - Point label font colour
-        pointLabelFontColor: "#666",
-
-        //Boolean - Whether to show a dot for each point
-        pointDot: true,
-
-        //Number - Radius of each point dot in pixels
-        pointDotRadius: 3,
-
-        //Number - Pixel width of point dot stroke
-        pointDotStrokeWidth: 1,
-
-        //Boolean - Whether to show a stroke for datasets
-        datasetStroke: true,
-
-        //Number - Pixel width of dataset stroke
-        datasetStrokeWidth: 2,
-
-        //Boolean - Whether to fill the dataset with a colour
-        datasetFill: true,
-
-        //Boolean - Whether to animate the chart
-        animation: true,
-
-        //Number - Number of animation steps
-        animationSteps: 60,
-
-        //String - Animation easing effect
-        animationEasing: "easeOutQuart",
-
-        //Function - Fires when the animation is complete
-        onAnimationComplete: null
-    }
-    var ctx = $("#radarGraph").get(0).getContext("2d");
-    var myRadarGraph = new Chart(ctx).Radar(data, options);
-}
 
 var getMovieName = function(element){
     return $(element).attr('data-movie-title');
