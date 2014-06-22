67ef90cf459744ffd46e3b9ed38b905d80bc9306
Richard Yadon <richardfyadon@gmail.com>
2014-06-22 06:35:09 -0400

    Updated UI, V1.0

--- static/js/main.js
+++ static/js/main.js
@@ -20,12 +20,12 @@ var bindEvents = function () {
 var getLineChartData = function (movieName) {
     if (movieName === undefined) {
         movieName = '';
-    }else {
+    } else {
         movieName = '?movie=' + encodeURIComponent(movieName);
     }
     console.log(movieName);
     $.ajax({
-        url: 'api/get_linechartdata'+ movieName,
+        url: 'api/get_linechartdata' + movieName,
         dataType: 'json',
         type: 'GET',
         success: function (data) {
@@ -55,10 +55,10 @@ var getLineChartData = function (movieName) {
             }
             max = max + 10;
             var stepWidth;
-            if(max < 8) {
+            if (max < 8) {
                 stepWidth = 1;
             } else {
-                stepWidth = Math.floor(max/8);
+                stepWidth = Math.floor(max / 8);
             }
             console.log(max);
             var options = {
@@ -119,14 +119,14 @@ var getLineChartData = function (movieName) {
                 onAnimationComplete: null
             };
             var ctx = $('#hourlyChart')[0].getContext("2d");
-            ctx.canvas.width = 400;
+            ctx.canvas.width = 350;
             ctx.canvas.height = 200;
             new Chart(ctx).Line(chartData, options);
         }
     });
 };
 
-var getSidebar = function() {
+var getSidebar = function () {
     $.ajax({
         url: 'api/get_sidebar',
         dataType: 'json',
@@ -223,9 +223,9 @@ var getDoughnutData = function () {
 
     });
 };
-    
-    
-var populateDoughnutLabels = function(names, data) {
+
+
+var populateDoughnutLabels = function (names, data) {
     $('#doughnutSpace').append('<p class="text-center" style="color: #F7464A; margin: 0 auto;">' + names[0] + ': ' + data[names[0]] + '</p>');
     $('#doughnutSpace').append('<p class="text-center" style="color: #718D8A; margin: 0 auto;">' + names[1] + ': ' + data[names[1]] + '</p>');
     $('#doughnutSpace').append('<p class="text-center" style="color: #9C9893; margin: 0 auto;">' + names[3] + ': ' + data[names[3]] + '</p>');
@@ -236,7 +236,7 @@ var populateDoughnutLabels = function(names, data) {
 var loadPosts = function () {
     // Get the movie name from data object
     originalMovieName = getMovieName(this);
-    movieName=  originalMovieName === undefined ? '' : '?movie='+encodeURIComponent(originalMovieName);
+    movieName = originalMovieName === undefined ? '' : '?movie=' + encodeURIComponent(originalMovieName);
 
     // Send request for posts
     $('#movieSidebar li').removeClass('active');
@@ -244,8 +244,9 @@ var loadPosts = function () {
 
     // Update graphs
     getLineChartData(originalMovieName);
+    getBarChartData(originalMovieName);
 
-    $.get( 'api/get_posts'+movieName, function( data ) {
+    $.get('api/get_posts' + movieName, function (data) {
         // Set new title
         movieName === '' ? $('#post-header').text('All Tweets') : $('#post-header').text(originalMovieName);
         // Clear table
@@ -253,7 +254,7 @@ var loadPosts = function () {
         table.empty();
         // Repopulate posts
         table.append('<tr><th>Time</th><th>Tweet</th><th>Confidence</th></tr>');
-        $.each(JSON.parse(data), function(idx, obj) {
+        $.each(JSON.parse(data), function (idx, obj) {
             if (obj.semantic === 0) {
                 if (obj.confidence > 85) {
                     table.append('<tr class="danger"><td style="width: 20%;">' + obj.time + '</td><td>' + obj.text + '</td><td>' + obj.confidence + '</td></tr></tbody>')
@@ -278,126 +279,122 @@ var loadPosts = function () {
     });
 };
 
-var getBarChartData = function () {
-//    $.ajax({
-//        url: 'api/get_doughnutdata',
-//        dataType: 'json',
-//        type: 'GET',
-//        success: function (data) {
-//
-//
-//        }
-//    });
-
-    var data1 = {
-        labels: ["Positive", "Negative", "Neutral"],
-        datasets: [
-            {
-                fillColor: "rgba(42,66,86,1)",
-                strokeColor: "rgba(94,123,119,.5)",
-                data: [60, 45, 30]
-            },
-            {
-                fillColor: "rgba(94,123,119,1)",
-                strokeColor: "rgba(42,66,86,.5)",
-                data: [86, 90, 78]
-            }
-        ]
-    }
-    var data3 = {
-        labels: ["Positive", "Negative", "Neutral"],
-        datasets: [
-             {
-                fillColor: "rgba(42,66,86,1)",
-                strokeColor: "rgba(94,123,119,.5)",
-                data: [60, 45, 30]
-            },
-            {
-                fillColor: "rgba(94,123,119,1)",
-                strokeColor: "rgba(42,66,86,.5)",
-                data: [86, 90, 78]
-            }
-        ]
-    }
-    var options = {
-
-        //Boolean - If we show the scale above the chart data
-        scaleOverlay: false,
-
-        //Boolean - If we want to override with a hard coded scale
-        scaleOverride: false,
-
-        //** Required if scaleOverride is true **
-        //Number - The number of steps in a hard coded scale
-        scaleSteps: null,
-        //Number - The value jump in the hard coded scale
-        scaleStepWidth: null,
-        //Number - The scale starting value
-        scaleStartValue: null,
-
-        //String - Colour of the scale line
-        scaleLineColor: "rgba(0,0,0,.1)",
-
-        //Number - Pixel width of the scale line
-        scaleLineWidth: 1,
-
-        //Boolean - Whether to show labels on the scale
-        scaleShowLabels: true,
-
-        //Interpolated JS string - can access value
-        scaleLabel: "<%=value%>",
-
-        //String - Scale label font declaration for the scale label
-        scaleFontFamily: "'Arial'",
-
-        //Number - Scale label font size in pixels
-        scaleFontSize: 12,
-
-        //String - Scale label font weight style
-        scaleFontStyle: "normal",
-
-        //String - Scale label font colour
-        scaleFontColor: "#666",
-
-        ///Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines: true,
-
-        //String - Colour of the grid lines
-        scaleGridLineColor: "rgba(0,0,0,.05)",
-
-        //Number - Width of the grid lines
-        scaleGridLineWidth: 1,
-
-        //Boolean - If there is a stroke on each bar
-        barShowStroke: true,
-
-        //Number - Pixel width of the bar stroke
-        barStrokeWidth: 2,
-
-        //Number - Spacing between each of the X value sets
-        barValueSpacing: 5,
-
-        //Number - Spacing between data sets within X values
-        barDatasetSpacing: 1,
-
-        //Boolean - Whether to animate the chart
-        animation: true,
-
-        //Number - Number of animation steps
-        animationSteps: 60,
-
-        //String - Animation easing effect
-        animationEasing: "easeOutQuart",
-
-        //Function - Fires when the animation is complete
-        onAnimationComplete: null
-
-    }
-    var ctx1 = $("#barGraph1").get(0).getContext("2d");
-    var myNewChart1 = new Chart(ctx1).Bar(data1, options);
+var randomNumber = function () {
+    return Math.floor(Math.random() * (99 - 65 + 1)) + 65;
 }
 
 
-var getMovieName = function(element){
+var getBarChartData = function (movieName) {
+    if (movieName === undefined) {
+        movieName = '';
+    } else {
+        movieName = '?movie=' + encodeURIComponent(movieName);
+    }
+    $.ajax({
+        url: 'api/get_bardata' + movieName,
+        dataType: 'json',
+        type: 'GET',
+        success: function (data) {
+            var barData = {
+                labels: ["Positive", "Negative", "Neutral"],
+                datasets: [
+                    {
+                        fillColor: "rgba(42,66,86,1)",
+                        strokeColor: "rgba(94,123,119,.5)",
+                        data: [data['positive'], data['negative'], data['neutral']]
+                    },
+                    {
+                        fillColor: "rgba(94,123,119,1)",
+                        strokeColor: "rgba(42,66,86,.5)",
+                        data: [randomNumber(), randomNumber(), randomNumber()]
+                    }
+                ]
+            }
+            var options = {
+
+                //Boolean - If we show the scale above the chart data
+                scaleOverlay: false,
+
+                //Boolean - If we want to override with a hard coded scale
+                scaleOverride: false,
+
+                //** Required if scaleOverride is true **
+                //Number - The number of steps in a hard coded scale
+                scaleSteps: null,
+                //Number - The value jump in the hard coded scale
+                scaleStepWidth: null,
+                //Number - The scale starting value
+                scaleStartValue: null,
+
+                //String - Colour of the scale line
+                scaleLineColor: "rgba(0,0,0,.1)",
+
+                //Number - Pixel width of the scale line
+                scaleLineWidth: 1,
+
+                //Boolean - Whether to show labels on the scale
+                scaleShowLabels: true,
+
+                //Interpolated JS string - can access value
+                scaleLabel: "<%=value%>",
+
+                //String - Scale label font declaration for the scale label
+                scaleFontFamily: "'Arial'",
+
+                //Number - Scale label font size in pixels
+                scaleFontSize: 12,
+
+                //String - Scale label font weight style
+                scaleFontStyle: "normal",
+
+                //String - Scale label font colour
+                scaleFontColor: "#666",
+
+                ///Boolean - Whether grid lines are shown across the chart
+                scaleShowGridLines: true,
+
+                //String - Colour of the grid lines
+                scaleGridLineColor: "rgba(0,0,0,.05)",
+
+                //Number - Width of the grid lines
+                scaleGridLineWidth: 1,
+
+                //Boolean - If there is a stroke on each bar
+                barShowStroke: true,
+
+                //Number - Pixel width of the bar stroke
+                barStrokeWidth: 2,
+
+                //Number - Spacing between each of the X value sets
+                barValueSpacing: 5,
+
+                //Number - Spacing between data sets within X values
+                barDatasetSpacing: 1,
+
+                //Boolean - Whether to animate the chart
+                animation: true,
+
+                //Number - Number of animation steps
+                animationSteps: 60,
+
+                //String - Animation easing effect
+                animationEasing: "easeOutQuart",
+
+                //Function - Fires when the animation is complete
+                onAnimationComplete: null
+
+            }
+            var ctx = $("#barGraph1").get(0).getContext("2d");
+            ctx.canvas.width = 350;
+            ctx.canvas.height = 200;
+            var myNewChart1 = new Chart(ctx).Bar(barData, options);
+        }
+    });
+
+
+};
+
+
+var getMovieName = function (element) {
     return $(element).attr('data-movie-title');
 };
