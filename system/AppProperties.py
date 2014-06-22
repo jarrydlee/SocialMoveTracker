e782840712e448a4d9ec8a4d6343ea5da7c05f39
Richard Yadon <richardfyadon@gmail.com>
2014-06-21 20:57:54 -0400

    Added Properties class

---
+++ system/AppProperties.py
@@ -0,0 +1,28 @@
+__author__ = 'tyadon'
+
+class MovieProperties:
+    MOVIES = [
+        "Identity Thief",
+        "Jurassic Park",
+        "Oblivion",
+        "Fast & Furious 6",
+        "The Purge",
+        "Despicable Me 2",
+        "R.I.P.D.",
+        "2 Guns",
+        "Kick-Ass 2",
+        "The World's End",
+        "Riddick",
+        "Rush",
+        "About Time",
+        "47 Ronin",
+        "Lone Survivor",
+        "Ride Along",
+        "Endless Love",
+        "Non-Stop",
+        "Neighbors",
+        "A Million Ways to Die in the West"
+    ]
+
+    def getMovieList(self):
+        return self.MOVIES
